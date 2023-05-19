20dfb8512ee2b3deb15e3bb834b4295526b43adb
nadale28 <nadale28@naver.com>
2023-05-19 11:35:51 +0900

    visual studio, node 기본 사용법

---
+++ start.js
@@ -0,0 +1,17 @@
+//좌측 확장 메뉴(ctrl+shift+x)에서 code runner 설치
+
+//Code Runner : js파일을 단축키로 쉽게 실행해 볼 수 있다.
+//이제 ctrl+alr+n 으로 js파일 실행 가능 
+
+//Live Server : 소스코드 수정할때마다 브라우저에 자동 반영하는 기능
+//우측 하단에 Go Live란 기능이 생겼음
+//html파일에서 go live 버튼을 클릭하면 브라우저에서 볼 수 있다.
+
+//nodejs 설치 후 터미널에서 실행 가능하다. 
+//ctrl+` : vscode 터미널 단축키
+//node start 명령어를 입력하면 터미널에서 js가 실행된다.
+
+const arr = [1,2,3];
+arr.forEach(console.log);
+
+
